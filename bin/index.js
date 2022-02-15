3aac036520e0d68ad341c3849490bafc3e0eca00
zhanghuaixiang <harri_1992@163.com>
2022-02-15 21:02:37 +0800

    1

---
+++ bin/index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+const program = require('commander');
+const setHelpOptions = require('../lib/core/help');
+const createCommander = require('../lib/core/create');
+// 设置版本号
+program.version(require('../../package.json').version);
+// 帮助和可选信息
+setHelpOptions();
+// 创建其他指令
+createCommander();
+program.parse(process.argv);
